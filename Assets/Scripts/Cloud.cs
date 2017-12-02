56b02697f878af00a2c6d02da10c38d57dc28181
Javi Cepa <javicepa@gmail.com>
2017-12-02 15:39:57 +0100

    Draw main manu image

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+	public Sprite[] cloudSprites; 
+	
+	void Start () {
+		Randomize();
+	}
+	
+	void Randomize() {
+		GetComponent<SpriteRenderer>().sprite = cloudSprites[Random.Range(0, cloudSprites.Length)];
+	}
+}
---
+++ Assets/Scripts/PixelPositionFixer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelPositionFixer : MonoBehaviour {
+	
+	void Update () {
+		if (!Application.isPlaying)
+		{
+			transform.localPosition = new Vector3(
+				Mathf.Round(transform.localPosition.x * 8f) / 8f,
+				Mathf.Round(transform.localPosition.y * 8f) / 8f,
+				transform.localPosition.z
+			);
+		}
+	}
+}
