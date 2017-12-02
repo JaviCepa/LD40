bbb6f8b677afa19d278f32bf940afba0c1cc243e
Javi Cepa <javicepa@gmail.com>
2017-12-02 10:17:50 +0100

    Add character sprites

---
+++ Assets/Scripts/Editor/SpritePostprocessor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SpritePostprocessor : AssetPostprocessor
+{
+
+    void OnPreprocessTexture()
+    {
+        if (assetImporter.assetPath.Contains("Resources/Tiles"))
+        {
+            TextureImporter importer = assetImporter as TextureImporter;
+            importer.anisoLevel = 0;
+            importer.spriteImportMode = SpriteImportMode.Multiple;
+            importer.spritePixelsPerUnit = 8;
+            importer.wrapMode = TextureWrapMode.Clamp;
+            importer.mipmapEnabled = false;
+            importer.filterMode = FilterMode.Point;
+            importer.textureCompression = TextureImporterCompression.Uncompressed;
+			
+        }
+		
+    }
+
+}
