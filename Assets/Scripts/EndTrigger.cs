82e405cf379995f1cd0c1f70dac76f1494a170d2
javicepa <javicepa@gmail.com>
2019-05-01 19:27:32 +0200

    Added backgrounds

--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class EndTrigger : TriggerArea
 {
 
+    public EndingTypes endingType;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameManager.LoadEnding(endingType);
+    }
 
 }
